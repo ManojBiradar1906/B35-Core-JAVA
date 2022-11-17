0f74494cc1aeaac4de8ffeeda3c896c91660179b
Admin <Admin@DESKTOP-NT7JJH5>
2022-11-17 12:25:41 +0530

    FIRST COMMIT

---
+++ Day1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,11 @@
+package org.tnsindia.gitdemo;
+
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TNS India foundation");
+
+	}
+
+}
