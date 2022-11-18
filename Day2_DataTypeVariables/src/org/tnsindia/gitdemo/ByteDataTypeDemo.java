8e65ec557972627ba5c94f9711c3772e39b3a916
Admin <mmbiradar1906@gmail.com>
2022-11-18 08:13:54 +0530

    First Commit

---
+++ Day2_DataTypeVariables/src/org/tnsindia/gitdemo/ByteDataTypeDemo.java
@@ -0,0 +1,29 @@
+package org.tnsindia.gitdemo;
+
+public class ByteDataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int xyz=128;
+		
+		long _abc=2147483648L;
+		System.out.println("The value is: "+xyz);
+		System.out.println("The value is: "+_abc);
+		char num='z';
+		float c=15.67f;
+		double d=41.9;
+		String str="Manoj";
+		boolean var=true;
+		int x=(int)12.5;//type-casting
+		float y=(float)34;
+		System.out.println("The value is: "+num);
+		System.out.println("The value is: "+c);
+		System.out.println("The value is: "+d);
+		System.out.println("The value is: "+str);
+		System.out.println("The value is: "+var);
+		System.out.println("The value is: "+x);
+		System.out.println("The value is: "+y);
+
+	}
+
+}
