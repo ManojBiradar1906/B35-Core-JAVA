dca02770a66beca17602b0f4d044895684bea312
Admin <mmbiradar1906@gmail.com>
2022-11-20 18:57:49 +0530

    Decision making

---
+++ Day3_assignments/src/org/tnsindia_decisionmaking/demo/decisionmaking_example1.java
@@ -0,0 +1,19 @@
+package org.tnsindia_decisionmaking.demo;
+
+public class decisionmaking_example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=11,b=17;
+		if(a>b)
+		{
+			System.out.println("a is greater than b");
+		}
+		else
+		{
+			System.out.println("b is greater than a");
+
+		}
+	}
+
+}
