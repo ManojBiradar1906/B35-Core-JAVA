1b047dadd5d036ec9350281ad1a57f596d541133
Admin <mmbiradar1906@gmail.com>
2022-11-20 19:03:48 +0530

    second commit

---
+++ Day3_assignments/src/org/tnsindia_decisionmaking/demo/decisionmaking_example2.java
@@ -0,0 +1,23 @@
+package org.tnsindia_decisionmaking.demo;
+
+public class decisionmaking_example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=11,b=7,c=23;
+		if(a>b && a>c)
+		{
+			System.out.println("a is greater than all");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is greater than all");
+
+		}
+		else
+		{
+			System.out.println("c is greater than all");
+		}
+	}
+
+}
